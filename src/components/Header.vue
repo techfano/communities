fd49a064edb28d7892b37cffd6a3ea10acdf96d2
techfano <mixc21@gmail.com>
2018-11-28 18:16:38 -0500

    primer commit

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <!--div class="brand">
+      
+    </div-->
+    <!--div class="option left">
+      <router-link to="/">
+        Home
+      </router-link>
+    </div>
+    <div class="option left">
+      <router-link to="/about">
+        About
+      </router-link>
+    </div-->
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    props: {
+      msg: String,
+    },
+  };
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <Header />
+    <Articles title="Articulos"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue';
+import Articles from '@/components/Articles.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Articles,
+    Header
+  },
+};
+</script>
